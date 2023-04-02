5c54c7e02228a6cd40e56a11cc03e6c745f284dd
devopseasylearning <info@devopseasylearning.com>
2023-04-02 00:20:57 -0400

    April 01

---
+++ MYSTUDY/file1.py
@@ -0,0 +1,38 @@
+# Displaying stuff
+
+# poem = "There once was a movie star icon \n who preferred to sleep with the light on. \n \
+#     They learned how to code \n a device that sure glowed \n and lit up the night using python!"
+# print (poem)
+
+# Expecting Input & Intro to Variables
+
+#collect name from user
+# name = input("What is your name? ")
+# display name
+# print (name)
+
+#update value
+# name = "Acheampong"
+# print (name)
+
+#concatenation
+firstname = input ("What's your first name? ")
+#print (firstname)
+lastname = input ("What's your last name? ")
+name = firstname + " " + lastname 
+country = input ("What's your country? ")
+country = country.upper()
+print ("\n Hello, " + name + ". You live in " + country)
+
+# Manipulating contents of a variable
+# message = "Hello World"
+# print (message.lower())
+# print (message.upper())
+# print (message.capitalize())
+# print (message.title())
+# print (message.strip())
+# print (message.rstrip())
+# print (message.lstrip())
+# print (message.count('o'))
+# print (message.find('o'))
+# print (message.swapcase ())
